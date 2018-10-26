52e4e4f6c633c24fb734cd915f38d4fa65ff2e80
tfabien <ftaysse@gmail.com>
2018-10-26 17:40:07 +0200

    Add "align" parameter, can be "left","center" or "right"

--- thumbor_text_filter/text.py
+++ thumbor_text_filter/text.py
@@ -14,10 +14,19 @@ class Filter(BaseFilter):
         BaseFilter.String,#color name see: http://pillow.readthedocs.io/en/4.0.x/reference/ImageColor.html#color-names
         BaseFilter.PositiveNumber, #font-size
         BaseFilter.String, #font-family
+		BaseFilter.String, #align
     )
-    def text(self, word, x, y, color, font_size, font_family="Tahoma"):
+    def text(self, word, x, y, color, font_size, font_family="Tahoma", align=""):
         image = self.engine.image
         usr_font = ImageFont.truetype(font_family, font_size)
-        drawer = ImageDraw.Draw(image)
-        drawer.text((x, y), word, fill=color, font=usr_font)
+        draw = ImageDraw.Draw(image)
+        w, h = draw.textsize(word, font=usr_font)
+        if align == 'center':
+            draw.text((x - (w / 2), y - (h / 2)), word, fill=color, font=usr_font)
+        elif align == 'left':
+            draw.text((x, y - (h / 2)), word, fill=color, font=usr_font)
+        elif align == 'right':
+            draw.text((x - w, y - (h / 2)), word, fill=color, font=usr_font)
+        else: 
+		    draw.text((x, y), word, fill=color, font=usr_font)
         self.engine.image = image
